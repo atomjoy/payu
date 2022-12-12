67640c251adf3f3f9296ab3b72ca7ef6b1023f8d
Atomjoy <atomjoy.official@gmail.com>
2022-12-12 20:19:16 +0100

    Payu Laravel
    Płatności PayU w Laravel. Jak utworzyć link do płatności za zamówienie w payu api.

--- database/migrations/2022_02_01_1_create_orders_table.php
+++ database/migrations/2022_02_01_1_create_orders_table.php
@@ -17,7 +17,7 @@ class CreateOrdersTable extends Migration
 			$table->id();
 			$table->uuid('uid')->unique()->index()->nullable(true);
 			$table->decimal('cost', 15, 2)->default(0.00);
-			$table->enum('payment_method', ['money', 'card', 'online'])->nullable()->default('money');
+			$table->enum('payment_method', ['money', 'card', 'online', 'pickup'])->nullable()->default('money');
 			$table->string('payment_gateway')->nullable(true);
 			$table->timestamps();
 			$table->softDeletes();
@@ -32,45 +32,5 @@ class CreateOrdersTable extends Migration
 	public function down()
 	{
 		Schema::dropIfExists('orders');
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function downTable()
-	{
-		Schema::enableForeignKeyConstraints();
-		Schema::dropIfExists('users');
-		Schema::disableForeignKeyConstraints();
-
-		if (Schema::hasTable('users')) {
-			// The "users" table exists...
-		}
-
-		if (Schema::hasColumn('users', 'phone')) {
-			Schema::table('users', function (Blueprint $table) {
-				$table->dropColumn('phone');
-				$table->dropColumn(['votes', 'avatar', 'location']);
-				$table->foreignUuid('user_id');
-				$table->string('name', 50)->nullable()->change();
-				$table->unique('email', 'unique_email');
-
-				$table->unsignedBigInteger('user_id');
-				$table->foreign('user_id')->references('id')->on('users');
-				$table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
-			});
-		}
-	}
-
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		Schema::defaultStringLength(191);
-	}
+	}	
 };
